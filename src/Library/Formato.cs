01ddf159b4dd38e593c5a55d134e915281be83a4
Levi de los Santos <levidlsantos@gmail.com>
2020-04-21 17:07:48 -0300

    Tarea1

---
+++ src/Library/Formato.cs
@@ -0,0 +1,17 @@
+﻿//Esta Clase se crea para quitar la responsabilidad a Program de resolver el Formato del Resultado.
+//Se utiliza en Patron Expert 
+
+using System;
+
+namespace Library
+{
+    public class Formato
+    {
+        
+        public static String ChangeFormat(String year)
+        {
+            return year.Substring(6) + "-" + year.Substring(3, 2) + "-" + year.Substring(0, 2);
+        }
+    }
+}
+
